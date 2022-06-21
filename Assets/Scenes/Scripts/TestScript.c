299ed39112a8fee7a748d5d55f84e5703639569c
JosueCaceda99 <josuecaceda@hotmail.com>
2022-06-21 18:09:04 -0300

    V.0.0.1
    Mi primer commit de Unity

---
+++ Assets/Scenes/Scripts/TestScript.c
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.collections.generic;
+using UnityEngine;
+
+public
+class TestScript : MonoBehaviour
+{
+    // Star is called before firs frame update
+    void Star()
+    {
+
+        Debug.log(¨HOLAAA MUNDOOOO¨);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
